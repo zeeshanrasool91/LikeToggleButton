86bb0787b5e2e1b3f893d29e6ae5243cdd32dc03
Zeeshan Rasool <zrasool@mbrhe.ae>
2019-01-27 15:09:47 +0400

    Adding Remaining files to Github

--- app/src/main/java/com/zeeshan/likeanimation/LikeToggleButton.kt
+++ app/src/main/java/com/zeeshan/likeanimation/LikeToggleButton.kt
@@ -1,21 +1,29 @@
 package com.zeeshan.likeanimation
 
+
 import android.content.Context
 import android.graphics.Canvas
+import android.graphics.drawable.Drawable
+import android.graphics.drawable.StateListDrawable
+import android.os.Build
 import android.support.v4.content.ContextCompat
 import android.util.AttributeSet
+import android.util.TypedValue
 import android.view.animation.Animation
 import android.view.animation.BounceInterpolator
 import android.view.animation.ScaleAnimation
 import android.widget.CompoundButton
 import android.widget.ToggleButton
 
+
 class LikeToggleButton : ToggleButton, CompoundButton.OnCheckedChangeListener {
 
-    private var favoriteBackground = 0
-    private var unFavoriteBackground = 0
+    private var favoriteBackground = -1
+    private var unFavoriteBackground = -1
+    private var likeColor = -1
+    private var unlikeColor = -1
 
-    internal var likeToggleListener: LikeToggleListener? = null
+    private var likeToggleListener: LikeToggleListener? = null
 
     fun setLikeToggleListener(likeToggleListener: LikeToggleListener) {
         this.likeToggleListener = likeToggleListener
@@ -41,8 +49,10 @@ class LikeToggleButton : ToggleButton, CompoundButton.OnCheckedChangeListener {
                 //likeToggleListener = (LikeToggleListener) this;
                 favoriteBackground = attrArray.getResourceId(R.styleable.LikeToggleButton_like_icon, favoriteBackground)
                 unFavoriteBackground = attrArray.getResourceId(R.styleable.LikeToggleButton_unlike_icon, unFavoriteBackground)
+                likeColor = attrArray.getResourceId(R.styleable.LikeToggleButton_like_color, likeColor)
+                unlikeColor = attrArray.getResourceId(R.styleable.LikeToggleButton_unlike_color, unlikeColor)
             } finally {
-                attrArray.recycle()
+                //attrArray.recycle()
             }
         }
     }
@@ -67,8 +77,9 @@ class LikeToggleButton : ToggleButton, CompoundButton.OnCheckedChangeListener {
 
     private fun renderView() {
         //update View Status Here
-        setBackgroundDrawable(ContextCompat.getDrawable(context, R.drawable.button_selector))
+        setToggleIcons()
         setOnCheckedChangeListener(this)
+
     }
 
     override fun onCheckedChanged(buttonView: CompoundButton, isChecked: Boolean) {
@@ -77,8 +88,63 @@ class LikeToggleButton : ToggleButton, CompoundButton.OnCheckedChangeListener {
             likeToggleListener!!.liked(isChecked)
         }
     }
+    private fun setToggleIcons(){
+        val states = arrayOf(intArrayOf(android.R.attr.state_checked), // checked
+                intArrayOf(-android.R.attr.state_checked) // unchecked
+        )
+        val drawables = arrayListOf(ContextCompat.getDrawable(context, if (favoriteBackground == -1) {
+            R.drawable.ic_favorite
+        } else {
+            favoriteBackground
+        }),
+                ContextCompat.getDrawable(context, if (unFavoriteBackground == -1) {
+                    R.drawable.ic_favorite_border
+                } else {
+                    unFavoriteBackground
+                }))
+        val toggleIconStates = StateListDrawable()
+        toggleIconStates.addState(states[0], drawables[0])
+        toggleIconStates.addState(states[1], drawables[1])
+        val sdk = android.os.Build.VERSION.SDK_INT
+        if (sdk < android.os.Build.VERSION_CODES.JELLY_BEAN) {
+            setBackgroundDrawable(toggleIconStates)
+        } else {
+            background = toggleIconStates
+        }
+        setToggleIconColors(drawables)
+    }
+
+    private fun setToggleIconColors(drawables: ArrayList<Drawable?>){
+        if (likeColor == -1) {
+            drawables[0]!!.setColorFilter(ContextCompat.getColor(context, getThemeAccentColor(context)), android.graphics.PorterDuff.Mode.SRC_IN)
+        } else {
+            drawables[0]!!.setColorFilter(ContextCompat.getColor(context, likeColor), android.graphics.PorterDuff.Mode.SRC_IN)
+        }
+
+        if (unlikeColor == -1) {
+            drawables[1]!!.setColorFilter(ContextCompat.getColor(context, getThemeAccentColor(context)), android.graphics.PorterDuff.Mode.SRC_IN)
+        } else {
+            drawables[1]!!.setColorFilter(ContextCompat.getColor(context, unlikeColor), android.graphics.PorterDuff.Mode.SRC_IN)
+        }
+    }
 
     interface LikeToggleListener {
         fun liked(isLike: Boolean)
     }
+
+
+    private fun getThemeAccentColor(context: Context): Int {
+        val colorAttr: Int
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
+            colorAttr = android.R.attr.colorAccent
+        } else {
+            //Get colorAccent defined for AppCompat
+            colorAttr = context.resources.getIdentifier("colorAccent", "attr", context.packageName)
+        }
+        val outValue = TypedValue()
+        context.theme.resolveAttribute(colorAttr, outValue, true)
+        return outValue.data
+    }
+
+
 }
